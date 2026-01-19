974b0422d28a461f86fe68bce7cdd9487df5ae56
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-19 13:02:06 +0300

    /

---
+++ FormatDemo.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Введите первое число");
+double num1 =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 =Convert.ToDouble(Console.ReadLine());
+double sum = num1 + num2;
+Console.WriteLine(sum);
